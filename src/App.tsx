f1089e7e8b6d48cca581494524152958502cc13f
Siddartha Tummala <sidddddd@amazon.com>
2025-06-03 18:19:19 -0700

    Initial stab

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <main className="bg-gray-600 w-[100%] h-full" >
+      <div id="title">
+        <h1 className="text-yellow-500 font-bold text-center">Connecting Words</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
